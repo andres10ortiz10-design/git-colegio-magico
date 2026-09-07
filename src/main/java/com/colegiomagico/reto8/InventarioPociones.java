7b8ac03061d235f3a5da2a88c7de41e78e98eb94
andres10ortiz10-design <andres10ortiz10@gmail.com>
2026-09-07 22:33:36 +0000

    feat: reto8 completado

--- src/main/java/com/colegiomagico/reto8/InventarioPociones.java
+++ src/main/java/com/colegiomagico/reto8/InventarioPociones.java
@@ -4,9 +4,14 @@ package com.colegiomagico.reto8;
  * Filtra la lista de ingredientes devolviendo solo aquellos que empiezan con 'A'.
  */
 public class InventarioPociones {
-
     public static java.util.List<String> filtrar(java.util.List<String> ingredientes) {
-        return new java.util.ArrayList<>(); // TODO: Cambia esto
+    java.util.List<String> filtrados = new java.util.ArrayList<>();
+    for (String ingrediente : ingredientes){
+if (ingrediente.startsWith("A")) {
+    filtrados.add(ingrediente);
+}
+    }
+    return filtrados;
     }
 
     public static void main(String[] args) {
